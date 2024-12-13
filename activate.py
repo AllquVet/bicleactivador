15d0370a732749b888b0e8d9bee850181c3a45de
AllquVet <allqu6571@gmail.com>
2024-12-13 16:45:17 -0500

    Update activate.py

--- activate.py
+++ activate.py
@@ -1,6 +1,7 @@
 import webbrowser
 import time
 import random as rd
+import streamlit as st
 def ejecutar_sitio_web(url, intervalo):
     """
     Abre un sitio web en el navegador predeterminado cada cierto tiempo.
@@ -10,16 +11,18 @@ def ejecutar_sitio_web(url, intervalo):
         intervalo (int): Intervalo en segundos entre cada apertura del sitio.
     """
     try:
-        v
+        valor_aleatorio=rd.choice([1.5,2,2.5,3])
+        st.header("Bot test")
         print(f"Iniciando apertura del, sitio web: {url} cada {intervalo} segundos. Presiona Ctrl+C para detener.")
         while True:
             webbrowser.open(url)
             time.sleep(intervalo)
+            
     except KeyboardInterrupt:
         print("\nEjecución detenida por el usuario.")
 
 # Configura la URL del sitio web y el intervalo de tiempo
 url_sitio = "https://allquwebservices.onrender.com"
-intervalo_segundos = 60  # Cambia esto al intervalo deseado en segundos
+intervalo_segundos = 90  # Cambia esto al intervalo deseado en segundos
 
 ejecutar_sitio_web(url_sitio, intervalo_segundos)
